318d357b72345e1a27c88e6b5c2e94cd6170adca
Christian Rocha <christian@rocha.is>
2024-11-27 07:25:56 -0500

    fix: minor example edits (#3)
    * docs(example): show help by default
    
    * docs(example): various copyedits

--- example/main.go
+++ example/main.go
@@ -10,34 +10,37 @@ import (
 )
 
 func main() {
-	cmd := &cobra.Command{
+	var cmd *cobra.Command
+	cmd = &cobra.Command{
 		Use:   "example [args]",
-		Short: "A simple example program!",
-		Long: `A simple example program!
+		Short: "An example program!",
+		Long: `A pretty silly example program!
 
-This is an example program we made for serpentine!`,
+It doesn’t really do anything, but that’s the point.`,
 		Example: `
 # Run it:
 example
 
-# Run it with some argument:
+# Run it with some arguments:
 example --name=Carlos -a -s Becker -a
 
-# Run a sub command with an argument:
+# Run a subcommand with an argument:
 example sub --async --foo=xyz --async arguments
 		`,
 		// Args: cobra.ArbitraryArgs,
-		Run: func(*cobra.Command, []string) {},
+		Run: func(*cobra.Command, []string) {
+			_ = cmd.Help()
+		},
 	}
 	var foo string
 	var bar int
-	var zaz float64
+	var baz float64
 	var d time.Duration
 	cmd.PersistentFlags().StringVarP(&foo, "surname", "s", "doe", "Your surname")
 	cmd.Flags().StringVar(&foo, "name", "john", "Your name")
 	cmd.Flags().DurationVar(&d, "duration", 0, "Time since your last commit")
 	cmd.Flags().IntVar(&bar, "age", 0, "Your age")
-	cmd.Flags().Float64Var(&zaz, "idk", 0.0, "I don't know")
+	cmd.Flags().Float64Var(&baz, "idk", 0.0, "I don't know")
 	cmd.Flags().BoolP("async", "a", false, "Run async")
 
 	_ = cmd.Flags().MarkHidden("age")
@@ -46,8 +49,10 @@ example sub --async --foo=xyz --async arguments
 
 	cmd.AddCommand(&cobra.Command{
 		Use:   "sub [command] [flags] [args]",
-		Short: "An example sub command",
+		Short: "An example subcommand",
 	})
+
+	// This is where the magic happens.
 	if err := serpentine.Execute(
 		context.Background(),
 		cmd,
